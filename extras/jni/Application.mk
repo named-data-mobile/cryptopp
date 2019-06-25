c7b2e7117a8c41f8e73a6b0ae61c4820de159635
Alexander Afanasyev <aa@cs.fiu.edu>
2019-06-25 12:11:19 -0400

    Update android build script

--- extras/jni/Application.mk
+++ extras/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_ABI := all
 
-APP_STL := gnustl_shared
-APP_CPPFLAGS += -fexceptions -frtti
-APP_PLATFORM := android-8
+APP_STL := c++_shared
+APP_CPPFLAGS += -fexceptions -frtti -std=c++14
+APP_PLATFORM := android-23
